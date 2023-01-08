df946869d34eaa265d62808add7eaa58c93b0c21
xumingyu528 <xumingyu528@gmail.com>
2023-01-08 23:49:25 +0800

    init project and add some pom.xml

---
+++ module01_monolith/customer-system/src/main/java/site/xmy/projects/cs/Application.java
@@ -0,0 +1,4 @@
+package site.xmy.projects.cs;
+
+public class Application {
+}
---
+++ module01_monolith/customer-system/src/main/java/site/xmy/projects/cs/entity/staff/CustomerStaff.java
@@ -0,0 +1,9 @@
+package site.xmy.projects.cs.entity.staff;
+
+import java.io.Serializable;
+
+@Data
+@EqualsAndHasCode(callSuper = false)
+@Accessors(chain = true)
+public class CustomerStaff implements Serializable {
+}
